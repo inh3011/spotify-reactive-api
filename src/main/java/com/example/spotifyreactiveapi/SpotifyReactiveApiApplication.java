e86dd7773a018ce83e027e85623e7333860de97b
inh3011 <inh3011@gmail.com>
2025-10-01 15:46:59 +0900

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/spotifyreactiveapi/SpotifyReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyreactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotifyreactiveapi/SpotifyReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyreactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
